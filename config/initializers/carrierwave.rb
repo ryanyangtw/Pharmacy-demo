25bde64154340cfd95ba386bead5160c68934067
Ryan <hiphubryan@gmail.com>
2014-09-11 22:06:49 +0800

    Fix: fix bug on amazon

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAJFK2O6UVG3SCIDUQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_secret_access_key  => 'Yfa6OeG85W3+gIw82SahWlpJJFVgt5e6tD3vVbSc'                        # required
   }
   config.fog_directory  = 'pharmacy-demo'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
+  #config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
